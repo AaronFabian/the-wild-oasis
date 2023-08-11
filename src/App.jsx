a90dba80f7b8459dc80a77dfc92373abc8d54045
A22DC527 <A22DC527@dhw.ac.jp>
2023-08-11 16:15:16 +0900

    checkpoint 333

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
-function App() {
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
 
-  return <div>Hello world</div>;
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <Row>
+        <Row type="horizontal">
+          <Heading as="h1">The wild oasis</Heading>
+
+          <div>
+            <Button>check in</Button>
+          </div>
+        </Row>
+
+        <Row>
+          <Heading as="h2">Form</Heading>
+          <form>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </Row>
+    </>
+  );
 }
 
-export default App
+export default App;
